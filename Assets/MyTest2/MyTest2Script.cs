60199a68a55ca3010f91796a55ad557168bdfaf6
TindleWei <tindle.wei@gmail.com>
2018-02-02 18:13:21 +0800

    Add MyTest3

--- Assets/MyTest2/MyTest2Script.cs
+++ Assets/MyTest2/MyTest2Script.cs
@@ -15,21 +15,10 @@ public class MyTest2Script : MonoBehaviour {
 		go = GameObject.CreatePrimitive (PrimitiveType.Sphere);
 		go.transform.localScale = new Vector3 (0.2f, 0.2f, 0.2f);
 		go.transform.position = new Vector3 (0f, 0f, -0.96f);
+		go.layer = LayerMask.NameToLayer ("PowerUI");
 
 		htmlDocument = GetComponent<WorldUIHelper> ().WorldUI.document;
-		
-//		var element1=GetComponent<WorldUIHelper> ().WorldUI.document.body.getElementsByClassName("button1");
-//		foreach (var element in element1) {
-//			element.ontouchmove = delegate(TouchEvent touchEvent) {
-//				print("Button clicked ontouchmove");
-//				//right
-//				go.transform.position += new Vector3(0.1f, 0, 0);
-//			};
-//			element.onmousedown = delegate (MouseEvent mouseEvent) {
-//				print ("button1 button1 button1");
-//				go.transform.position += new Vector3(0.1f, 0, 0);
-//			};
-//		}
+		//htmlDocument = GetComponent<Manager>().Document;
 
 		var ele_left = htmlDocument.body.getElementById ("btn_left");
 		ele_left.onmousedown = delegate (MouseEvent mouseEvent) {
@@ -43,19 +32,19 @@ public class MyTest2Script : MonoBehaviour {
 			go.transform.position += new Vector3(0.1f, 0, 0);
 		};
 
-		var ele_color = htmlDocument.body.getElementByName ("btn_color");
+		var ele_color = htmlDocument.body.getElementById ("btn_color");
 		ele_color.onmousedown = delegate (MouseEvent mouseEvent) {
 			print ("btn_color btn_color btn_color");
 			go.GetComponent<MeshRenderer>().material.color = new Color(Random.Range(0, 2), 
 				Random.Range(0, 2), Random.Range(0, 2));
 		};
 
-		var ele_bigger = htmlDocument.body.getElementByName ("btn_bigger");
+		var ele_bigger = htmlDocument.body.getElementById ("btn_bigger");
 		ele_bigger.onmousedown = delegate (MouseEvent mouseEvent) {
 			print ("btn_bigger btn_bigger btn_bigger");
 			go.transform.localScale += new Vector3(0.1f, 0.1f, 0.1f);
 		};
-		ele_bigger.onmouseup = OnElementMouseUp;
+		//ele_bigger.onmouseup = OnElementMouseUp;
 			
 	}
 
--- Assets/PowerUI/Source/Engine/Tags/a_modle.cs
+++ Assets/PowerUI/Source/Engine/Tags/a_modle.cs
@@ -20,17 +20,60 @@ namespace PowerUI{
 	[Dom.TagName("a_model")]
 	public class HtmlA1Element:HtmlElement{
 
+		GameObject go;
+
 		public override void OnTagLoaded(){
 			string innterName =htmlDocument.innerText;
 
-			GameObject go = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-			go.name = htmlDocument.innerHTML;
-			go.transform.position = new Vector3(2f, 0f, 0f);
+			go = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+			go.name = "a_model";
+			go.layer = LayerMask.NameToLayer ("PowerUI");
 
 			Debug.Log ("HtmlA1Element Loaded");
-			go.transform.parent = GameObject.Find ("Container").transform;
+			//go.transform.parent = GameObject.Find ("Container").transform;
 		}
-		
+
+		public override void move(float x, float y, float z) {
+			if (go == null)
+				return;
+			go.transform.position += new Vector3(x, y, z);
+			Debug.Log ("a_model move");
+
+		}
+
+		public override void changeColor(float r, float g, float b, float a) {
+			if (go == null)
+				return;
+			// ????0~1
+			Color color = new Color (r, g, b, a);
+			go.GetComponent<MeshRenderer> ().material.color = color;
+		}
+
+		public override void scale(float factor) {
+			if (go == null)
+				return;
+			go.transform.localScale = go.transform.localScale * factor;
+		}
+	}
+
+
+	public partial class HtmlElement{
+
+		public virtual void move(float x, float y, float z) {
+
+		}
+
+		public virtual void changeColor(float r, float g, float b, float a) {
+			
+		}
+
+		public virtual void scale(float factor) {
+
+		}
+
+		public float getRandomValue() {
+			return Random.value;
+		}
 	}
 	
 }
